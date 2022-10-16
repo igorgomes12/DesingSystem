55ea4c96885b4bc622e7f451dae9e77fa1e5bbbc
igorgomes12 <igorgomesigla@gmail.com>
2022-10-16 17:50:29 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import './styles/global.css';
+
+export function App() {
+
+
+  return (
+    <>
+      <h1 className="font-bold text-2xl text-gray-400"> Hello Word!</h1>
+
+      <button className="bg-cyan-300 font-medium px-4 py-2 rounded text-white hover:bg-cyan-500">
+        Enviar
+      </button>
+    </>
+  )
+}
+
